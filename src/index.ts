1b384de722c0ab8d829ec77b712754bb26c3dc42
EsmSanchezVictor <100707976+EsmSanchezVictor@users.noreply.github.com>
2022-03-24 20:10:20 -0300

    final de clase

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
-import { Application, Sprite, Loader, Container, Point } from 'pixi.js'
-
+import { Application,  Loader,  } from 'pixi.js'
+import { asseets } from './assets';
+import { Scene } from './Scene';
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -39,44 +40,13 @@ window.dispatchEvent(new Event("resize"));
 
 
 // una forma const myLoader = new Loader();
-Loader.shared.add({ url: "./pngegg.png", name: "loki" });
-Loader.shared.add({ url: "./fez.png", name: "Fez" });
-
+Loader.shared.add(asseets); //importo desde assets
 
 Loader.shared.onComplete.add(()=>{
-	const loki: Sprite = Sprite.from("loki");
-	const fez: Sprite = Sprite.from("Fez");
-	
-	fez.scale.set(0.4,0.4);
-	fez.position.set(100,-90);
-	
-	loki.anchor.set(0);
-	
-	const lokiconfez: Container =new Container();
-		
-	
-	lokiconfez.addChild(loki); 
-	lokiconfez.addChild(fez); 
-
-	
-	lokiconfez.scale.set(0.5);
-	lokiconfez.x=200;
-	lokiconfez.y=200;
-
-	fez.toGlobal(new Point()); //el point lo convierte en coor globales-  gorro decime donde queda tu origen
-	fez.parent.toGlobal(fez.position);// padre de fez decime la posicion global del gorro
-	
-	/*1280*720  mitad de la pantalla  640*360
-
-	const aux=fez.parent.toLocal(new Point(640,360));
-	
-	fez.position.copyFrom(aux);
-	
-	fez.position.x=aux.x;
-	fez.position.y=aux.y;*/
-
-	app.stage.addChild(lokiconfez);
-	
+	const myScene=new Scene();
+	app.stage.addChild(myScene);
 });
 
 Loader.shared.load();
+
+
